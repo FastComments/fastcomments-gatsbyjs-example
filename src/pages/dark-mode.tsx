9cc8ead4288f241120d00612f43adeb78876f3a3
winrid <winrid@gmail.com>
2020-08-12 09:38:41 -0700

    dark mode example

---
+++ src/pages/dark-mode.tsx
@@ -0,0 +1,14 @@
+// noinspection ES6UnusedImports
+import React, {useState} from "react"
+import {FastCommentsCommentWidget} from 'fastcomments-react'; // Here we include the FastComments library.
+
+export default function DarkMode() {
+    const [hasDarkBackground, setHasDarkBackground] = useState(false);
+    return (
+        <div style={{backgroundColor: hasDarkBackground ? 'black' : 'white'}}>
+            <button onClick={() => setHasDarkBackground(!hasDarkBackground)}>Has dark
+                background? {hasDarkBackground ? 'true' : 'false'}</button>
+            <FastCommentsCommentWidget tenantId="nYrnfYEv" hasDarkBackground={hasDarkBackground}/>
+        </div>
+    )
+}
